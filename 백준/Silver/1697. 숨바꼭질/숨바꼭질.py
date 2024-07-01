a366a986fcc5e7bdfc32a496e109f34143941ace
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2024-07-01 17:32:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,30 +2,24 @@ from collections import deque
 
 # input
 n, k = map(int, input().split())
-visited = [-1] * (100000 + 1)
+visited = [-1] * 100001
 
-# solve (bfs)
+# solve (bfs) - 큐에 넣을 때 방문 처리 (중복 방지)
+# bfs 초기화
 queue = deque([n])
 visited[n] = 0
 
+# bfs 탐색
 while queue:
     x = queue.popleft()
 
     if x == k:
         break
 
-    if 0 <= x-1 <= 100000 and visited[x-1] == -1:
-        visited[x-1] = visited[x] + 1
-        queue.append(x-1)
-
-    if 0 <= x+1 <= 100000 and visited[x+1] == -1:
-        visited[x+1] = visited[x] + 1
-        queue.append(x+1)
-
-    if 0 <= 2*x <= 100000 and visited[2*x] == -1:
-        visited[2*x] = visited[x] + 1
-        queue.append(2*x)
-
+    for next_x in [x-1, x+1, 2*x]:
+        if 0 <= next_x <= 100000 and visited[next_x] == -1:
+            visited[next_x] = visited[x] + 1
+            queue.append(next_x)
 
 # print
 print(visited[k])
