a6a80d1fd95b3831b830a1f18537226b04680232
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2024-07-04 20:04:04 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,2 @@
-n = int(input())
-p = list(map(int, input().split()))
-p.sort()
-
-answer = 0
-for i in range(n):
-    answer += p[i] * (n-i)
-
-print(answer)
+n, p = int(input()), sorted(map(int, input().split()))
+print(sum([p[i]*(n-i) for i in range(n)]))
