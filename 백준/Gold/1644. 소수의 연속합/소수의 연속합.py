f11865e7063ec6a68a6fab9020ce04fe0bf97381
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2024-07-29 18:41:20 +0900

    [Gold III] Title: 소수의 연속합, Time: 1684 ms, Memory: 93620 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,38 +1,32 @@
 n = int(input())
-is_prime = [False, False] + [True] * (n-1)
 
-for num in range(2, n+1):
+# 에라토스테네스의 체를 사용하여 소수 찾기
+is_prime = [False, False] + [True] * (n - 1)  # 인덱스 0과 1은 소수가 아니므로 False로 시작
+
+for num in range(2, n + 1):
     if is_prime[num]:
-        for x in range(num*2, n+1, num):
+        for x in range(num * 2, n + 1, num):
             is_prime[x] = False
 
-prime_numbers = []
+# 소수 리스트 생성
+prime_numbers = [num for num in range(2, n + 1) if is_prime[num]]
 
-for num in range(2, n+1):
-    if is_prime[num]:
-        prime_numbers.append(num)
-
-f, b = 0, 0
+# 투 포인터를 사용하여 연속된 소수들의 합으로 n을 만드는 경우의 수를 찾기
 answer = 0
-target = n
 length = len(prime_numbers)
+target = n
 
-if prime_numbers:
-    total = prime_numbers[0]
+f = 0
+total = 0
 
-    while f < length:
-        if total == target:
-            answer += 1
-            total -= prime_numbers[f]
-            f += 1
-        elif total > target:
-            total -= prime_numbers[f]
-            f += 1
-        else:  # total < target
-            if b < length - 1:
-                b += 1
-                total += prime_numbers[b]
-            else:
-                break
+for b in range(length):
+    total += prime_numbers[b]
+
+    while total > target and f <= b:
+        total -= prime_numbers[f]
+        f += 1
+
+    if total == target:
+        answer += 1
 
 print(answer)
