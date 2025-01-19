e04b5c7c0667b3bd720507ff18be14b4e1b8b497
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-01-20 01:42:59 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,10 +1,11 @@
-a = [1]
+a = 1
 r = 0
 n = int(input())
 for i in range(1, n+1):
-    a.append(i*a[i-1])
+    a *= i
 
-for x in str(a[n])[::-1]:
+a = str(a)
+for x in a[::-1]:
     if x == '0':
         r += 1
     else:
