a006aeb51c9975947c9ead7bdf359eb9ae9867a3
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-03-20 16:04:46 +0900

    [level 2] Title: 전화번호 목록, Time: 943.47 ms, Memory: 261 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+def solution(phone_book):
+    answer = True
+    
+    phone_prefix = dict()
+    
+    for phone_number in phone_book:
+        for i in range(1, len(phone_number)):
+            phone_prefix[phone_number[:i]] = True
+    
+    for phone_number in phone_book:
+        if phone_prefix.get(phone_number) == True:
+            answer = False
+            break
+            
+    return answer
