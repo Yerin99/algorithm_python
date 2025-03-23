47bf1161e606017b682b2ad8fec3b6c18a95ea79
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-03-23 20:50:27 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,24 @@
+def solution(n, lost, reserve):
+    answer = n
+    students = [0]*n
+    
+    for l in lost:
+        students[l-1] -= 1
+    
+    for r in reserve:
+        students[r-1] += 1
+    
+    for i in range(n):
+        if students[i] == -1:
+            if i-1 >= 0 and students[i-1] == 1:
+                students[i] += 1
+                students[i-1] -= 1
+            elif i+1 < n and students[i+1] == 1:
+                students[i] += 1
+                students[i+1] -= 1
+                
+    for student in students:
+        if student == -1:
+            answer -= 1
+    
+    return answer
