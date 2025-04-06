1d1155d649dc8433abb44528f7845b08c64b7e46
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-06 22:27:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,35 +1,26 @@
 import heapq
+from collections import defaultdict
 
 def solution(genres, plays):
     answer = []
-    
-    song_dict = dict() 
     genre_dict = dict()
-    popular_dict = dict()
-    
-    genre_heap = []
+    song_dict = defaultdict(list)
     
     n = len(plays)
-    
     for sid in range(n):
         genre, play = genres[sid], plays[sid]
-        song_dict[sid] = [genre, play] 
         genre_dict[genre] = genre_dict.get(genre, 0) + play
-        
-        popular_dict[genre] = popular_dict.get(genre, [])
-        heapq.heappush(popular_dict[genre], [-play, sid])
+        heapq.heappush(song_dict[genre], [-play, sid])  # 최대 힙
     
-    for genre, total_play in genre_dict.items():
-        heapq.heappush(genre_heap, [-total_play, genre])
+    genre_heap = [[-total_play, genre] for genre, total_play in genre_dict.items()]
+    heapq.heapify(genre_heap)
     
     while genre_heap:
         _, hot_genre = heapq.heappop(genre_heap)
+        num_songs = min(2, len(song_dict[hot_genre]))
         
-        for i in range(2):
-            _, sid = heapq.heappop(popular_dict[hot_genre])
-            answer.append(sid) 
-            
-            if not popular_dict[hot_genre]:
-                break
-            
+        for _ in range(num_songs):
+            _, sid = heapq.heappop(song_dict[hot_genre])
+            answer.append(sid)
+    
     return answer
