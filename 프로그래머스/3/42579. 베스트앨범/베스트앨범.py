af30cd258b6c1d93102bef4d7b8004f03d195c5d
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-06 22:47:59 +0900

    [level 3] Title: 베스트앨범, Time: 0.25 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,26 +1,32 @@
-import heapq
 from collections import defaultdict
 
+
+class Album:
+    def __init__(self, genre, play, track):
+        self.genre = genre
+        self.play = play
+        self.track = track
+
+    def __lt__(self, other):
+        if self.play == other.play:
+            return self.track < other.track  
+        return self.play > other.play
+
+    
 def solution(genres, plays):
     answer = []
-    genre_dict = dict()
+    
+    genre_dict = defaultdict(int)
     song_dict = defaultdict(list)
-    
-    n = len(plays)
-    for sid in range(n):
-        genre, play = genres[sid], plays[sid]
-        genre_dict[genre] = genre_dict.get(genre, 0) + play
-        heapq.heappush(song_dict[genre], [-play, sid])  # 최대 힙
-    
-    genre_heap = [[-total_play, genre] for genre, total_play in genre_dict.items()]
-    heapq.heapify(genre_heap)
-    
-    while genre_heap:
-        _, hot_genre = heapq.heappop(genre_heap)
-        num_songs = min(2, len(song_dict[hot_genre]))
-        
-        for _ in range(num_songs):
-            _, sid = heapq.heappop(song_dict[hot_genre])
-            answer.append(sid)
-    
+
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        genre_dict[g] += p
+        song_dict[g].append(Album(g, p, i))
+
+    genre_order = sorted(genre_dict.items(), key=lambda x: x[1], reverse=True)
+
+    for genre, _ in genre_order:
+        sorted_songs = sorted(song_dict[genre],)
+        answer.extend([a.track for a in sorted_songs[:2]])
+
     return answer
