b1927442677f960cdb391163bd831d0db458698b
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-08 14:47:44 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 43.25 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 70.84 ms
+메모리: 9.32 MB, 시간: 43.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 07일 17:48:48
+2025년 04월 08일 14:47:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -2,30 +2,31 @@ from collections import deque
 
 def solution(bridge_length, max_weight, truck_weights):
     answer = 0
-    
+
     num_of_trucks = len(truck_weights)
     bridge_queue = deque([0]*bridge_length)
     truck_queue = deque(truck_weights)
+
     sum_bridge = 0
-    
-    
+
     while truck_queue:
         truck = truck_queue.popleft()
-        
+
         while True:
             something = bridge_queue.popleft()
             answer += 1
-            
+
             if something != 0:  # it's truck!
                 sum_bridge -= something
-            
+
             if sum_bridge + truck <= max_weight:
+                # new truck is now on the road
                 sum_bridge += truck
-                bridge_queue.append(truck)
+                bridge_queue.append(truck)  
                 break
             else:
                 bridge_queue.append(0)
-        
-    answer += bridge_length         
+
+    answer += bridge_length  # time for last truck
     
     return answer
