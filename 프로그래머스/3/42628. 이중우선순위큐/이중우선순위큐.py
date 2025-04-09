3641b711d0e418b016c48250a444825132e294f5
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-09 23:01:25 +0900

    [level 3] Title: 이중우선순위큐, Time: 156.19 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,24 @@
+import heapq
+from collections import deque
+
+def solution(operations):
+    answer = [0, 0]
+    queue = []
+    
+    for operation in operations:
+        order, num = operation.split()
+        num = int(num)
+        
+        if order == "I":
+            queue.append(num)
+            queue.sort()
+        elif order == "D":
+            if num == 1:
+                queue = queue[:-1]
+            else:
+                queue = queue[1:]
+    
+    if queue:
+        answer = queue[-1], queue[0]
+    
+    return answer
