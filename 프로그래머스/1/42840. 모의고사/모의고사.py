07c78328592f8a802a114db5055a6269574225f9
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-16 20:06:11 +0900

    [level 1] Title: 모의고사, Time: 6.73 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -5,18 +5,15 @@ def solution(answers):
         [2, 1, 2, 3, 2, 4, 2, 5],
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ]
-    
     scores = [0, 0, 0]
     
     for i in range(len(answers)):
         for student in range(3):
             if patterns[student][i % len(patterns[student])] == answers[i]:
                 scores[student] += 1
-            
-    max_score = max(scores)
     
     for student in range(3):
-        if scores[student] == max_score:
+        if scores[student] == max(scores):
             answer.append(student + 1)
     
     return answer
