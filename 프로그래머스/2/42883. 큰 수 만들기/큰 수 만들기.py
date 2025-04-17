b06ed687f55349e8ae4254778cca4573f256580c
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-17 23:13:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 180.60 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,26 +1,31 @@
 def solution(number, k):
     answer = ''
-    stack = []
-    number = list(number)[::-1]
-    org_length = len(number)
     
-    while number:
-        num = number.pop()
-        while stack and stack[-1] < num:
+    n = len(number)
+    stack = []
+    
+    # 앞에서부터 숫자 하나씩 순회
+    for i in range(n):
+        current = number[i]
+        
+        '''
+        push 하기 전에:
+        스택 마지막 숫자가 지금 숫자보다 작고,
+        k > 0이면 pop (숫자 제거), k -= 1
+        '''
+        
+        while stack and stack[-1] < current and k > 0:
             stack.pop()
             k -= 1
-                
-            if k == 0:
-                stack.append(num)    
-                stack += number[::-1]
-                break
-        
-        if k == 0:
-            break
-            
-        if len(stack) < org_length - k:
-            stack.append(num)    
+
+        stack.append(current)
+           
+    '''
+    다 돌고 나서 k가 남아 있으면?
+    → 그만큼 뒤에서 제거
+    '''
     
-    answer = ''.join(stack)
+    answer = ''.join(stack[:n-k])
+
     
     return answer
