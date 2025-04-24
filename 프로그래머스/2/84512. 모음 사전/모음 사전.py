56a46d0a41193275dc6c17ef9ba55865fac854a5
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-04-24 11:30:29 +0900

    [level 2] Title: 모음 사전, Time: 2.57 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -2,22 +2,22 @@ from itertools import product
 from functools import cmp_to_key
 
 
-def compare(x, y):
-    aeiou = ['A', 'E', 'I', 'O', 'U']
-    d = dict()
+# def compare(x, y):
+#     aeiou = ['A', 'E', 'I', 'O', 'U']
+#     d = dict()
 
-    for i in range(5):
-        d[aeiou[i]] = i
+#     for i in range(5):
+#         d[aeiou[i]] = i
     
-    for i in range(min(len(x), len(y))):
-        if d[x[i]] < d[y[i]]:
-            return -1
-        elif d[x[i]] > d[y[i]]:
-            return 1
-        else:
-            continue
+#     for i in range(min(len(x), len(y))):
+#         if d[x[i]] < d[y[i]]:
+#             return -1
+#         elif d[x[i]] > d[y[i]]:
+#             return 1
+#         else:
+#             continue
     
-    return len(x) - len(y)
+#     return len(x) - len(y)
 
 
 def solution(word):
@@ -29,7 +29,7 @@ def solution(word):
         for e in product(aeiou, repeat=i):
             dictionary.append("".join(list(e)))
     
-    dictionary.sort(key=cmp_to_key(compare))
+    dictionary.sort()
     answer = dictionary.index(word) + 1
     
     return answer
