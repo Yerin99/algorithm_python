4394f1b3f2d09582eb9433430e2e1b00ba7198ce
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-10-16 18:57:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.12 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,28 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
+    answer = -1
     
-    queue = deque()
     n, m = len(maps), len(maps[0])
-    start = (0, 0, 1)
+    visited = [[0]*m for _ in range(n)]
     
-    queue.append(start)
-    ds = [[1, 0], [0, 1], [0, -1], [-1, 0]]
+    start = (0, 0, 1)
+    visited[0][0] = 1
+    queue = deque([start])
+    d = [[1, 0], [0, 1], [-1, 0], [0, -1]]
     
     while queue:
-        x, y, dist = queue.popleft()
+        y, x, nums = queue.popleft()
         
-        if maps[y][x] == 1:
-            maps[y][x] = dist
+        if x == m-1 and y == n-1:
+            answer = nums
+            break
         
-            for dx, dy in ds:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n:
-                    queue.append((nx, ny, dist + 1))
-                
-    if maps[n-1][m-1] > 1:
-        answer = maps[n-1][m-1]
-    else:
-        answer = -1
-    
+        for dx, dy in d:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < m and 0 <= ny < n:
+                if not visited[ny][nx] and maps[ny][nx]:
+                    queue.append((ny, nx, nums+1))
+                    visited[ny][nx] = 1
     
     return answer
