88c26f21a0b8a755f029c45db26cbdb1c35a4c36
Yerin Park <63496777+Yerin99@users.noreply.github.com>
2025-10-24 17:09:54 +0900

    [level 3] Title: 네트워크, Time: 0.57 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,17 @@
 def solution(n, computers):
+    def dfs(node):
+        visited[node] = True
+        for i in range(n):
+            if not visited[i] and computers[node][i]:
+                dfs(i)
+    
     answer = 0
 
     visited = [False]*n
     
     for i in range(n):
         if not visited[i]:
-            stack = [i]
-            while stack:
-                node = stack.pop()
-                if not visited[node]:
-                    visited[node] = True
-                    for j in range(n):
-                        if node != j and computers[node][j]:
-                            stack.append(j)
+            dfs(i)
             answer += 1
     
     return answer
